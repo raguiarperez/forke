073fef5c5e6ee7c8263eaaa5ee7e81db92439028
rafa2 <rafa2@LAPTOP-SPV2MGHO>
2018-11-29 09:22:26 +0100

    Aasdjo

--- src/javaapplication8/JavaApplication8.java
+++ src/javaapplication8/JavaApplication8.java
@@ -17,6 +17,7 @@ public class JavaApplication8 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola");
+        System.out.println("cola");
     }
     
 }
